b2203ac58c5fd12fafeb0e957b9f763836e418c5
Robbie A <alamantus+github@gmail.com>
2023-07-11 00:57:51 -0600

    Set clientId and token from environment vars

--- tina/config.js
+++ tina/config.js
@@ -5,13 +5,10 @@ import { bookLists } from "./templates/bookLists";
 import { posts } from "./templates/posts";
 import { siteConfig } from "./templates/siteConfig";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-
 export default defineConfig({
-  branch,
-  clientId: "09d59101-3dac-45f8-b0ea-9b83fc07ce5a", // Get this from tina.io
-  token: "40b508ae6891ca84a04c968c62af806d8cf845a2", // Get this from tina.io
+  branch: 'main',
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
   client: { skip: true },
   build: {
     outputFolder: "manage-site",
